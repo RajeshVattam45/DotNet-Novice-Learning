36ee2b6c8dc4567847a612993e2438c55aa5e80b
RajeshVattam45 <vattamrajesh45@gmail.com>
2024-12-17 15:21:02 +0530

    MVC implimentation

---
+++ MvcApplication/Controllers/FruitController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApplication.Models;
+
+namespace MvcApplication.Controllers
+{
+    [Route("Fruits")]
+    public class FruitController : Controller
+    {
+        public object Index()
+        {
+            var fruits = new List<Fruit>
+            {
+                new Fruit { Id = 1, Name = "Apple", Color = "Red" },
+                new Fruit { Id = 2, Name = "Banana", Color = "Yellow" },
+                new Fruit { Id = 3, Name = "Orange", Color = "Orange" },
+                new Fruit { Id = 4, Name = "Grapes", Color = "Green" },
+                new Fruit { Id = 5, Name = "Blueberry", Color = "Blue" }
+            };
+
+            return View(fruits);
+        }
+    }
+}
---
+++ MvcApplication/Views/Fruit/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcApplication.Views.Fruit
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
