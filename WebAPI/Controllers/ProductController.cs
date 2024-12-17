f59f2fd24c22a581deb3a29fb66512a2d454ed34
RajeshVattam45 <vattamrajesh45@gmail.com>
2024-12-17 12:48:38 +0530

    Web API application

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        // Displaying Products.
+        private static readonly List<string> Products = new List<string>
+        {
+        "Laptop", "Tablet", "Smartphone"
+        };
+
+        // GET: api/product
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(Products);
+        }
+
+        // GET: api/product/1
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            if (id < 0 || id >= Products.Count)
+                return NotFound("Product not found");
+
+            return Ok(Products[id]);
+        }
+    }
+}
